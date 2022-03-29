75f3594aa25795ee7c81a69f08af2a17e7ac57b3
CarryTheToolio <89408537+CarryTheToolio@users.noreply.github.com>
2022-03-29 12:18:18 -0400

    Add files via upload

---
+++ bank_balance.py
@@ -0,0 +1,21 @@
+customer_name = input("Welcome to Friendly Farms Bank. What's your name? ")
+
+starting_balance = (float(5000.25))
+
+print(f"Hello {customer_name}, Your starting balance is {starting_balance}")
+
+pay_check = float(input("How much of your paycheck would you like to deposit? $"))
+
+expenditure_item = input("What was bought? ")
+
+expenditure = float(input(f"How much did {expenditure_item} cost: $"))
+
+def checking_balance(user_name, balance, deposits, expense_item, expense_amount):
+    user_name
+    balance
+    deposits
+    expense_item
+    expense_amount
+ending_balance = (starting_balance + pay_check - expenditure)
+
+print(f"The final total comes out to {ending_balance}")
